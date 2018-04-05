4bbadc49da9345806086b58004e48e36c9ed6f33
jiyinyiyong <jiyinyiyong@gmail.com>
2018-04-05 11:20:25 +0800

    initializing page

---
+++ README.md
@@ -0,0 +1,13 @@
+
+ClojureScript, beginners' home page
+----
+
+> No offending. I'm just trying to explore if such a page is meaningful for beginners.
+
+### Workflow
+
+Workflow https://github.com/mvc-works/calcit-workflow
+
+### License
+
+MIT
---
+++ src/app/comp/container.cljs
@@ -0,0 +1,20 @@
+
+(ns app.comp.container
+  (:require [hsl.core :refer [hsl]]
+            [respo-ui.core :as ui]
+            [respo.macros
+             :refer
+             [defcomp cursor-> action-> mutation-> <> div button textarea span]]
+            [verbosely.core :refer [verbosely!]]
+            [respo.comp.space :refer [=<]]
+            [reel.comp.reel :refer [comp-reel]]
+            [respo-md.comp.md :refer [comp-md]]
+            [app.schema :refer [dev?]]))
+
+(defcomp
+ comp-container
+ (reel)
+ (let [store (:store reel), states (:states store)]
+   (div
+    {:style (merge ui/global ui/row)}
+    (when dev? (cursor-> :reel comp-reel states reel {})))))
