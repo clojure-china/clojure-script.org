608bd87c7cd4cfb53c85b7c5c48e89a7686690cb
jiyinyiyong <jiyinyiyong@gmail.com>
2018-04-06 01:35:12 +0800

    basic styles to make page look sane

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -12,10 +12,23 @@
             [app.schema :refer [dev?]]))
 
 (defcomp
+ comp-footer
+ ()
+ (div
+  {:style {:background-color "#292d2e", :height 200}}
+  (div
+   {:style {:width 800, :margin :auto, :padding "16px 0"}}
+   (a
+    {:style {:color (hsl 240 80 80), :text-decoration :none},
+     :href "https://github.com/clojure-china/clojure-script.org",
+     :target "_blank"}
+    (<> "Site built with ClojureScript & Respo.")))))
+
+(defcomp
  comp-header
  ()
  (div
-  {:style {:background-color (hsl 0 0 0), :padding 8}}
+  {:style {:padding 8}}
   (div
    {:style (merge ui/row-parted {:width 800, :margin :auto})}
    (div
@@ -36,7 +49,7 @@
  comp-resources
  ()
  (div
-  {}
+  {:style {:width 800, :margin :auto}}
   (comp-md-block
    "\n### ClojureScript and Clojure\n\nClojureScript has almost the same syntax, same jar files for release libraries.\n\n### ClojureScript and npm\n\nYou can import code from npm modules. You can do js interop to call JavaScript code.\n\n### Learn ClojureScript\n\nLet me gather some links.\n"
    {})))
@@ -45,51 +58,79 @@
  comp-showcase
  ()
  (div
-  {:width 800}
+  {:class-name "showcase", :style (merge ui/row-center {:padding "16px"})}
+  (comp-md-block
+   "\n```clojure\n(ns app.main (:require [\"http\" :as http]))\n\n(defonce *visits (atom {}))\n\n(defn handler [req res]\n  (swap! *visits update (.-url req) inc)\n  (.writeHead res 200 #js {\"Content-Type\" \"text/html\"})\n  (.end res\n    (str \"<pre>Visits \" (pr-str @*visits) \"</pre>\")))\n\n(defn main! []\n  (let [server (http/createServer handler)]\n    (.listen server 3000)\n    (println \"server listening on 3000\")))\n\n(main!)\n```"
+   {:style {:background-color :transparent,
+            :font-family ui/font-code,
+            :color :white,
+            :padding 8}})
+  (=< 16 nil)
+  (comp-md-block
+   "ClojureScript is cool.\n\n* Feature A\n* Feature B\n* Feature C, just like that\n\ncomes with more..."
+   {:style {:color :white}})))
+
+(defcomp
+ comp-tool-card
+ (info)
+ (div
+  {:style (merge
+           ui/row-parted
+           {:border (str "1px solid " (hsl 0 0 90)),
+            :padding "8px 16px",
+            :border-radius "8px",
+            :margin-top 16})}
   (div
-   {:class-name "showcase", :style (merge ui/row-center)}
-   (comp-md-block
-    "\n```clojure\n(ns app.main (:require [\"http\" :as http]))\n\n(defonce *visits (atom {}))\n\n(defn handler [req res]\n  (swap! *visits update (.-url req) inc)\n  (.writeHead res 200 #js {\"Content-Type\" \"text/html\"})\n  (.end res\n    (str \"<pre>Visits \" (pr-str @*visits) \"</pre>\")))\n\n(defn main! []\n  (let [server (http/createServer handler)]\n    (.listen server 3000)\n    (println \"server listening on 3000\")))\n\n(main!)\n```"
-    {:style {:background-color (hsl 0 0 0),
-             :font-family ui/font-code,
-             :color :white,
-             :padding 8}})
-   (comp-md-block
-    "ClojureScript is cool.\n\n* Feature A\n* Feature B\n* Feature C, just like that\n\ncomes with more..."
-    {}))))
+   {}
+   (div {} (a {:href (:url info), :target "_blank"} (<> (:name info))))
+   (div {} (<> (:description info))))
+  (if (some? (:logo info))
+    (div
+     {:style {:background-image (str "url(" (:logo info) ")"),
+              :background-size :cover,
+              :background-position :center,
+              :width 40,
+              :height 40}}))))
 
 (defcomp
  comp-tools
  ()
  (div
-  {}
+  {:style {:width 800, :margin :auto}}
   (comp-md-block
    "There are tools for compiling ClojureScript, which is unlike Webpack.."
    {})
   (div
-   {:style (merge ui/row-center {:width :auto})}
-   (div
-    {:style {:width 120,
-             :height 120,
-             :background-image "url(http://cdn.tiye.me/logo/lumo.png)",
-             :background-size :cover,
-             :background-position :center}})
-   (div
-    {:style {:width 120,
-             :height 120,
-             :background-image "url(http://cdn.tiye.me/logo/shadow-cljs.png)",
-             :background-size :cover,
-             :background-position :center}})
-   (div {} (<> "Figwheel")))))
+   {:style (merge {:width :auto})}
+   (comp-tool-card
+    {:name "shadow-cljs",
+     :logo "http://cdn.tiye.me/logo/shadow-cljs.png",
+     :url "http://shadow-cljs.org",
+     :description "ClojureScript compilation made easy."})
+   (comp-tool-card
+    {:name "Lumo",
+     :logo "http://cdn.tiye.me/logo/lumo.png",
+     :url "http://lumo-cljs.org",
+     :description "Fast, cross-platform, standalone ClojureScript environment."})
+   (comp-tool-card
+    {:name "Figwheel",
+     :logo nil,
+     :url "https://github.com/bhauman/lein-figwheel",
+     :description "Figwheel builds your ClojureScript code and hot loads it into the browser as you are coding!"}))))
 
 (defcomp
  comp-container
  (reel)
  (let [store (:store reel), states (:states store)]
    (div
-    {:style (merge ui/global)}
-    (comp-header)
-    (comp-showcase)
+    {:style (merge ui/global {:background-color :white})}
+    (div
+     {:style {:background-image "linear-gradient(to right, #0f2242, #2452a1)"}}
+     (comp-header)
+     (comp-showcase))
+    (=< nil 32)
     (comp-tools)
     (comp-resources)
+    (=< nil 16)
+    (comp-footer)
     (when dev? (cursor-> :reel comp-reel states reel {})))))
